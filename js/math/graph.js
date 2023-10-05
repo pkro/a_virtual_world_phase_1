8e1a2681f55bcaa4444df430e95d912e45832fe9
pkro <pkrombach@web.de>
2023-10-05 07:26:19 +0200

    after lesson 1

--- js/math/graph.js
+++ js/math/graph.js
@@ -7,6 +7,61 @@ class Graph {
         this.segments = segments;
     }
 
+    tryAddPoint(p) {
+        if(!this.containsPoint(p)) {
+            this.addPoint(p);
+            return true;
+        }
+        return false;
+    }
+
+    addPoint(point) {
+        this.points.push(point);
+    }
+
+    containsPoint(point) {
+        return this.points.find(p => p.equals(point));
+    }
+
+    tryAddSegment(seg) {
+        if (!this.containsSegment(seg) && !seg.p1.equals(seg.p2)) {
+            this.addSegment(seg);
+            return true;
+        }
+        return false;
+    }
+
+    addSegment(seg) {
+        this.segments.push(seg)
+    }
+
+    containsSegment(seg) {
+        return this.segments.find(s=>s.equals(seg));
+    }
+
+    removeSegment(seg) {
+        this.segments.splice(this.segments.indexOf(seg), 1);
+        //this.segments = this.segments.reduce((acc, _, idx) => index === idx ? [...acc] : [...acc, this.segments[idx]], []);
+    }
+
+    removePoint(point) {
+        const segs = this.getSegmentsWithPoint(point);
+        for (const seg of segs) {
+            this.removeSegment(seg);
+        }
+        this.points.splice(this.points.indexOf(point), 1);
+
+    }
+
+    getSegmentsWithPoint(point) {
+        return this.segments.filter(seg => seg.includes(point));
+    }
+
+    dispose() {
+        this.segments = [];
+        this.points = [];
+    }
+
     draw(ctx) {
         for (const seg of this.segments) {
             seg.draw(ctx);
@@ -16,4 +71,5 @@ class Graph {
             point.draw(ctx);
         }
     }
+
 }
--- js/primitives/segment.js
+++ js/primitives/segment.js
@@ -1,10 +1,18 @@
 class Segment {
-    constructor(p1,p2) {
+    constructor(p1, p2) {
         this.p1 = p1;
         this.p2 = p2;
     }
 
-    draw(ctx, width=2, color="black") {
+    equals(seg) {
+        return this.includes(seg.p1) && this.includes(seg.p2);
+    }
+
+    includes(point) {
+        return this.p1.equals(point) || this.p2.equals(point);
+    }
+
+    draw(ctx, width = 2, color = "black") {
         ctx.beginPath();
         ctx.lineWidth = width;
         ctx.strokeStyle = color;
